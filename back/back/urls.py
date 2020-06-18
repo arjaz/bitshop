768d02fe7fa72e2d9be58e9e749e31c4a3973247
arjaz <arjaz@protonmail.com>
2020-06-18 21:26:26 +0300

    Clean up code and add postShop mutation to the schema

--- back/back/urls.py
+++ back/back/urls.py
@@ -1,21 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from graphene_django.views import GraphQLView
-from shop import views
 from shop.schema import schema
-
-router = routers.DefaultRouter()
-router.register('customers', views.CustomerViewSet)
-router.register('groups', views.GroupViewSet)
-router.register('shops', views.ShopViewSet)
-router.register('products', views.ProductViewSet)
-router.register('wallets', views.WalletViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls',
-                              namespace='rest_framework')),
-    path('api/', include(router.urls)),
     path('graphql/', GraphQLView.as_view(schema=schema, graphiql=True))
 ]
--- back/shop/schema.py
+++ back/shop/schema.py
@@ -42,4 +42,55 @@ class Query(graphene.ObjectType):
         return Product.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+class ShopMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        slug = graphene.String(required=True)
+
+    shop = graphene.Field(ShopType)
+
+    def mutate(self, info, name, slug):
+        wallet = Wallet(value=0)
+        wallet.save()
+
+        shop = Shop(name=name, slug=slug, wallet=wallet)
+        shop.save()
+
+        # TODO: set the current user as a holder
+        holder = Customer.objects.all().first()
+        shop.holders.add(holder)
+
+        return ShopMutation(shop=shop)
+
+
+class ProductMutation(graphene.Mutation):
+    class Arguments:
+        # id = graphene.ID()
+        name = graphene.String(required=True)
+        slug = graphene.String(required=True)
+        stock = graphene.Int(required=True)
+        price = graphene.Int(required=True)
+        shopId = graphene.ID(required=True)
+
+    product = graphene.Field(ProductType)
+
+    def mutate(self, info, name, slug, stock, price, shopId):
+        shop = Shop.objects.get(pk=shopId)
+        # TODO: Get category as well
+        category = Category.objects.all().first()
+        product = Product(name=name,
+                          slug=slug,
+                          stock=stock,
+                          price=price,
+                          shop=shop,
+                          category=category)
+        product.save()
+        return ProductMutation(product=product)
+
+
+class Mutation(graphene.ObjectType):
+    post_product = ProductMutation.Field()
+    post_shop = ShopMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
